d925031ed9e8cc0c60cb965e0108c3c6cfdd2337
Spm-Eng <dlspm0512@gmail.com>
2021-11-29 15:23:46 +0800

    feat: index sticky-popup activity-popover

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,2 +1,24 @@
-"use strict";
-//# sourceMappingURL=all.js.map
+// TODO: sticky-popup
+document.getElementById("sticky-popup").addEventListener("click", togglePopup);
+
+// document.querySelectorAll('#sticky-popup').forEach(item => {
+//   item.addEventListener('click', togglePopup(e));
+// })
+// var stickyPopup = document.querySelector("#sticky-popup");
+// for( let i = 0 ; i < stickyPopup.length ; i++) {
+//   stickyPopup[i].addEventListener("click", togglePopup());
+// }
+
+function togglePopup() {
+  console.log('click');
+  var element = document.getElementById("sticky-popup");
+  element.classList.toggle("open");
+  // item.classList.toggle("open");
+  
+  var type = document.querySelector(".dot-menu");
+  if(type.classList.contains("active") === true){
+      type.classList.remove("active");
+    }else{
+      type.classList.add("active");
+    }
+}
