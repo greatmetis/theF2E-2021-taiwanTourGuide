9e68fc740e5855fa8419fba2fbd63b062efe0c1a
Spm-Eng <dlspm0512@gmail.com>
2021-12-04 00:11:32 +0800

    Update 2021-12-03T16:11:29.855Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,19 @@
 "use strict";
 
-// dog-eating
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+window.addEventListener('load', load); // dog-eating
+
 var activity = document.getElementById('activity-popover-content');
 var activityDefaule = document.getElementById('activity-popover-defaule');
 activityDefaule.addEventListener('click', function () {
@@ -169,7 +182,17 @@ function addTogglePopup() {
 ;
 
 function togglePopup(e) {
-  e.currentTarget.classList.toggle("open");
+  var currentCard = e.currentTarget;
+  var currentId = currentCard.getAttribute("data-id");
+
+  var stickyPopupArr = _toConsumableArray(stickyPopup);
+
+  stickyPopupArr.forEach(function (item) {
+    if (item.getAttribute("data-id") !== currentId) {
+      item.classList.remove("open");
+    }
+  });
+  currentCard.classList.toggle("open");
 }
 
 ;
