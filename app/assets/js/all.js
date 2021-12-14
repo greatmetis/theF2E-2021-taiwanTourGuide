890ec574ff9a86c5d333b5f5c8d256aee66fc17d
Metis Teng <greatmetis@gmail.com>
2021-12-14 16:23:46 +1000

    Feature:connect side bar animation with popup card

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -24,32 +24,32 @@ window.addEventListener('resize', function () {
 function main(){
   get_activity();
   render_stickyPopup();
-  userSelection = new UserSelection(selectedCity,selectedCategory)
-  watch_userSelectedValue()
+  userSelection = new UserSelection(selectedCity,selectedCategory) // object to store user selections
+  watch_userSelectedValue() // start watching user selected values
 };
 main();
 
-// 監聽radio button, if it's changed change value inside the object
+// watch radio button, if it's changed, modify the values inside the object
 function watch_userSelectedValue(){
-  const cityBtnGroup = document.querySelector(".city-btn-group");
-  const activityBtnGroup = document.querySelector(".activity-btn-group");
-  cityBtnGroup.addEventListener('change',function(){
-    userSelection.city = selectedCity;
-    console.log(userSelection)
+  const cityBtnGroup = document.querySelectorAll(".city-btn-group");
+  const activityBtnGroup = document.querySelectorAll(".activity-btn-group");
+
+  cityBtnGroup.forEach(item=>{
+    item.addEventListener('change',function(){
+      userSelection.city = selectedCity;
+      console.log(userSelection)
+    })
   })
-  activityBtnGroup.addEventListener('change',function(){
-    userSelection.category = selectedCategory;
-    console.log(userSelection)
+
+  activityBtnGroup.forEach(item=>{
+    item.addEventListener('change',function(){
+      userSelection.category = selectedCategory;
+      console.log(userSelection)
+    })
   })
 }
 
-
-
-
-
-
-
-// dog-eating
+// 活動 Togggle
 const activity = document.getElementById('activity-popover-content');
 const activityDefault = document.getElementById('activity-popover-default');
 activity.addEventListener('click',(e)=>{
@@ -62,92 +62,7 @@ dogEating.addEventListener('click', () => {
   activity.classList.toggle("active");
 });
 
-// ===== Progress bar ===== //
-//TODO: dogScooter
 
-var prev = document.getElementById('prev');
-var next = document.getElementById('next');
-var circleItems = document.querySelectorAll('.circle-item');
-var circleItemNumbers = document.querySelectorAll('.circle-item-number');
-var progress = document.getElementById('progress');
-var actives = document.querySelectorAll('.active');
-var dogScooter = document.getElementById('dog-scooter');
-var currentActive = 1;
-var itemNum = 4; // next按鈕行為
-
-next.addEventListener('click', function () {
-  currentActive++; // 讓currentActive數值與 itemNum 同步
-
-  if (currentActive > itemNum) {
-    currentActive = itemNum;
-  }
-
-  nextUpdate();
-}); // prev按鈕行為
-
-prev.addEventListener('click', function () {
-  currentActive--; // 讓currentActive數值與 itemNum 同步
-
-  if (currentActive < 1) {
-    currentActive = 1;
-  }
-
-  prevUpdate();
-});
-
-function progressWidth() {
-  // 計算progress的長度（預設為0%）初始值：分母是4 - 1，分子是1 - 1
-  // 第一次按next: 分母是4 - 1，分子是2 - 1，相當於33%
-  // 第二次按next: 分母是4 - 1，分子是3 - 1，相當於66%
-  // 第三次按next: 分母是4 - 1，分子是4 - 1，相當於100%
-  var widthNum = ((currentActive - 1) / (itemNum - 1)) * 100;
-  progress.style.height = widthNum + '%'; // dogScooter.style.bottom = widthNum + '%';
-
-  dogScooter.style.bottom = 'calc('
-    .concat(widthNum, '% - ')
-    .concat(30 + (currentActive - 1) * 10, 'px)'); // dogScooter.style.transform = `translateY(-${widthNum}%) scaleX(-1)`;
-
-  dogScooter.style.transform = 'scaleX(-1)';
-}
-
-function btnDisable() {
-  // 判斷何時要將按鈕做disable處理
-  if (currentActive <= 1) {
-    prev.disabled = true;
-  } else if (currentActive === itemNum) {
-    next.disabled = true;
-  } else {
-    prev.disabled = false;
-    next.disabled = false;
-  }
-}
-
-function prevUpdate() {
-  // set finish
-  circleItems[currentActive - 1].classList.remove('done');
-  circleItems[currentActive - 1].classList.add('active');
-  circleItemNumbers[currentActive - 1].classList.add('invisible');
-  circleItemNumbers[currentActive - 1].classList.add('done');
-  circleItemNumbers[currentActive - 1].classList.remove('done'); // set now
-
-  circleItems[currentActive].classList.remove('active');
-  circleItemNumbers[currentActive].classList.remove('invisible');
-  progressWidth();
-  btnDisable();
-}
-
-function nextUpdate() {
-  // set finish
-  circleItems[currentActive - 2].classList.add('done');
-  circleItems[currentActive - 2].classList.remove('active');
-  circleItemNumbers[currentActive - 2].classList.remove('invisible');
-  circleItemNumbers[currentActive - 2].classList.add('done'); // set now
-
-  circleItemNumbers[currentActive - 1].classList.add('invisible');
-  circleItems[currentActive - 1].classList.add('active');
-  progressWidth();
-  btnDisable();
-} // TODO: color-theme
 
 function load() {
   var $html = document.querySelector('html');
--- app/assets/js/modules/stickyPopup.js
+++ app/assets/js/modules/stickyPopup.js
@@ -1,3 +1,5 @@
+import {setStage,prevStage} from "./animation";
+
 const stickyPopup = document.getElementsByClassName('sticky-popup');
 const stickyPopupHeader = document.getElementsByClassName('popup-header');
 const stickyPopups = document.querySelector('.sticky-popups');
@@ -74,11 +76,6 @@ export function render_stickyPopup(){
 
     // insert activity tags
     activityTagColumn.forEach((item) => {
-        // strActivity += /*html*/`
-        //     <ul class="bottom-popup-tag">
-        //     ${createActivityTags(item)}
-        //     </ul>
-        // `;
         strActivity += /*html*/`
         <div class="activity-btn-group" >
         ${createActivityTags(item)}
@@ -93,10 +90,10 @@ export function render_stickyPopup(){
 
         // dynamically insert city tags
         cityTags = item.cities;
-        cityTags.forEach((item,index)=>{
+        cityTags.forEach((item)=>{
             let tempCityHtml = /*html*/`
-                <input type="radio" class="btn-check" name="city-btnradio" id="city-btnradio-${index}" autocomplete="off" checked>
-                <label class="btn btn-outline-dark city-tags" for="city-btnradio-${index}">${item}</label>
+                <input type="radio" class="btn-check" name="city-btnradio" id="city-btnradio-${item}" autocomplete="off">
+                <label class="btn btn-outline-dark city-tags" for="city-btnradio-${item}">${item}</label>
             `
             strCity += tempCityHtml;
         });
@@ -116,14 +113,15 @@ export function render_stickyPopup(){
 
     //**  Add event listener **//
 
-    // stickyPopup toggle
     for (let i = 0; i < stickyPopupHeader.length; i++) {
         stickyPopupHeader[i].addEventListener('click', togglePopup);
-    };
-    // city && activity selections
-    addEventToCityTag();
-    addEventToActivityTag();
-}
+    };// stickyPopup toggle
+    
+    addEventToCityTag();// city selections
+    addEventToActivityTag();// activity selections
+    addEventToSelectedRegion();// region selection
+    addEventToFinalisedBtn(); // 下一步（開始推播）
+};
 
 export function computed_stickyPopupWidth(){
     console.log(document.body.clientWidth, document.body.clientWidth - 280);
@@ -133,56 +131,55 @@ export function computed_stickyPopupWidth(){
 function createStickyPopups({id,region,city,tempeture,weather,shortDescription, cityTags, activityTags}){
     return /*html*/`
         <li class="w-20">
-            <div
-            class="sticky-popup open_sticky_popup popup-content-bounce-in-up" data-id="${id}">
-            <div class="popup-header d-flex justify-content-between align-items-center">
-                <h2 class="popup-title">${region}</h2>
-                <div class="dot-menu">
-                <div class="dot dot1"></div>
-                <div class="dot line1"></div>
-                <div class="dot line2"></div>
-                <div class="dot dot2"></div>
+            <div class="sticky-popup open_sticky_popup popup-content-bounce-in-up" data-id="${id}">
+                <div class="popup-header d-flex justify-content-between align-items-center">
+                    <h2 class="popup-title">${region}</h2>
+                    <div class="dot-menu">
+                    <div class="dot dot1"></div>
+                    <div class="dot line1"></div>
+                    <div class="dot line2"></div>
+                    <div class="dot dot2"></div>
+                    </div>
                 </div>
-            </div>
-            <div class="popup-content-trapezoid"></div>
-            <div class="popup-content popup-body-city">
-                <p class="">我想去...</p>
-                <div class="overflow-scroll bottom-popup-tag city-btn-group mb-3 me-n6" role="btn-group" aria-label="city selections">
-                ${cityTags}
+                <div class="popup-content-trapezoid"></div>
+                <div class="popup-content popup-body-city">
+                    <p class="">我想去...</p>
+                    <div class="overflow-scroll bottom-popup-tag city-btn-group mb-3 me-n6" role="btn-group" aria-label="city selections">
+                    ${cityTags}
+                    </div>
+                    <div class="popup-body-activity disabled">
+                        <p class="">我想要...</p>
+                        <div class="overflow-scroll bottom-popup-tag mb-3 me-n6" role="btn-group" aria-label="activity selections">
+                        ${activityTags}
+                        </div>
+                        <button
+                            class="btn btn-secondary w-100 rounded-0 border border-1 rounded-2 mt-3 finalised-selection-btn">
+                            <span class="align-middle text-black">下一步</span>
+                            <img src="assets/images/arrow_expand.svg" class="arrow-hover ms-1" alt="arrow icon">
+                        </button>
+                    </div>
                 </div>
-                <div class="popup-body-activity">
-                <p class="">我想要...</p>
-                <div class="overflow-scroll bottom-popup-tag mb-3 me-n6" role="btn-group" aria-label="activity selections">
-                    ${activityTags}
+                <div class="popup-content border border-2 border-dark border-bottom-0 px-6 py-6">
+                    <button class="btn btn-lg btn-yellow border border-2 lh-sm text-black mb-2">${tempeture}&#8451;</button>
+                    <div class="d-flex justify-content-center align-items-center">
+                    <h3 class="fs-l mb-1">${weather}</h3>
+                    <img src="assets/images/${weather}.svg" class="ms-1" alt="${weather} icon">
+                    </div>
+                    <span class="d-block text-muted fs-5">${city}今日早上</span>
                 </div>
-                <button
-                    class="btn btn-secondary w-100 rounded-0 border border-1 rounded-2 mt-3">
-                    <span class="align-middle text-black">下一步</span>
+                <div class="popup-content popup-body-description border border-2 border-dark border-top-0 px-6 pb-6">
+                    <p class="mb-6 fw-6">
+                    ${shortDescription}
+                    </p>
+                    <button class="d-flex justify-content-center align-items-center bg-secondary w-100 rounded-2 border py-1 px-4 region-btn">
+                    選城市
                     <img src="assets/images/arrow_expand.svg" class="arrow-hover ms-1" alt="arrow icon">
-                </button>
+                    </button>
                 </div>
-            </div>
-            <div class="popup-content border border-2 border-dark border-bottom-0 px-6 py-6">
-                <button class="btn btn-lg btn-yellow border border-2 lh-sm text-black mb-2">${tempeture}&#8451;</button>
-                <div class="d-flex justify-content-center align-items-center">
-                <h3 class="fs-l mb-1">${weather}</h3>
-                <img src="assets/images/${weather}.svg" class="ms-1" alt="${weather} icon">
-                </div>
-                <span class="d-block text-muted fs-5">${city}今日早上</span>
-            </div>
-            <div class="popup-content popup-body-description border border-2 border-dark border-top-0 px-6 pb-6">
-                <p class="mb-6 fw-6">
-                ${shortDescription}
-                </p>
-                <button class="d-flex justify-content-center align-items-center bg-secondary w-100 rounded-2 border py-1 px-4">
-                選城市
-                <img src="assets/images/arrow_expand.svg" class="arrow-hover ms-1" alt="arrow icon">
-                </button>
-            </div>
             </div>
         </li>
         `;
-}
+};
 function togglePopup(e) {
     let currentCard = e.currentTarget.parentNode;
     let currentId = currentCard.getAttribute('data-id');
@@ -190,42 +187,50 @@ function togglePopup(e) {
 
     stickyPopupArr.forEach((item) => {
         if (item.getAttribute('data-id') !== currentId) {
-        item.classList.remove('open','city');
+        item.classList.remove('open');
         selectedCity = '';
         selectedCategory ='';
         watch_tagStatus();
         }
     });
-        
     currentCard.classList.toggle('open');
-    // TODO: deciding if we want to have city description
-    currentCard.classList.toggle('city');
-}
+    
+    
+};
 function addEventToCityTag() {
     const cityTagBtn = document.querySelectorAll(".city-tags");
     for(let i = 0; i < cityTagBtn.length; i++){
         cityTagBtn[i].addEventListener('click', function(){
             selectedCity = this.textContent;
             watch_tagStatus();
+            setStage(2);
         })
     }
     
-}
+};
 function addEventToActivityTag(){
     const activityTagBtn = document.querySelectorAll(".activity-tag");
      // activiy selection
     for(let i = 0; i < activityTagBtn.length; i++){
         activityTagBtn[i].addEventListener('click', function(){
-            selectedCategory = this.firstChild.nextElementSibling.textContent
+            selectedCategory = this.firstChild.nextElementSibling.textContent;
+            setStage(3);
         })
     }
+};
+function addEventToFinalisedBtn(){
+    const finalisedSelectionBtn = document.querySelector(".finalised-selection-btn");
+    finalisedSelectionBtn.addEventListener('click',function(){
+        setStage(4);
+    })
 }
+
 // FIXME: a horizontal line in the middle between activity tags
 function createActivityTags(items) {
     let str = "";
     items.forEach((item) => {
         str += /*html*/`
-            <input type="radio" class="btn-check" name="activity-btnradio" id="activity-btnradio-${item}" autocomplete="off" checked>
+            <input type="radio" class="btn-check" name="activity-btnradio" id="activity-btnradio-${item}" autocomplete="off">
             <label class="btn btn-outline-dark activity-tag" for="activity-btnradio-${item}">
             <span class="align-middle">${item}</span>
             <img src="assets/images/${item}.svg" class="ms-1" alt="${item} icon">
@@ -233,7 +238,16 @@ function createActivityTags(items) {
         `
     });
     return str;
-}
+};
+function addEventToSelectedRegion(){
+    const regionBtn = document.querySelectorAll(".region-btn");
+    regionBtn.forEach((item,index)=>{
+        item.addEventListener('click',function(){
+            stickyPopup[index].classList.add("city");
+            setStage(1);
+        })
+    })
+};
 function watch_tagStatus(){
     const activityTagSection = document.querySelectorAll(".popup-body-activity");
     if(!selectedCity){
@@ -241,6 +255,8 @@ function watch_tagStatus(){
     }else{
         activityTagSection.forEach(btn=>btn.classList.remove("disabled"))
     }
-}
+};
+
+
 
 export {selectedCity,selectedCategory}
